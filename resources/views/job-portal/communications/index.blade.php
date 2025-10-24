fc2ac44a5ccc82e0af7284facf3a6b75395d58cb
Zahid Ur Rehman <94544058+ZahidNiazi@users.noreply.github.com>
2025-10-24 18:52:53 +0500

    pagination done into communications
    done

--- resources/views/job-portal/communications/index.blade.php
+++ resources/views/job-portal/communications/index.blade.php
@@ -295,7 +295,48 @@
 
                     <!-- Pagination -->
                     <div class="d-flex justify-content-center mt-4">
-                        {{ $communications->links() }}
+                        <nav>
+                            <ul class="pagination">
+                                <!-- Previous Page Link -->
+                                @if ($communications->onFirstPage())
+                                    <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
+                                        <span class="page-link" aria-hidden="true"><i class="bi bi-chevron-left"></i></span>
+                                    </li>
+                                @else
+                                    <li class="page-item">
+                                        <a class="page-link" href="{{ $communications->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">
+                                            <i class="bi bi-chevron-left"></i>
+                                        </a>
+                                    </li>
+                                @endif
+
+                                <!-- Pagination Elements -->
+                                @foreach ($communications->getUrlRange(1, $communications->lastPage()) as $page => $url)
+                                    @if ($page == $communications->currentPage())
+                                        <li class="page-item active" aria-current="page">
+                                            <span class="page-link">{{ $page }}</span>
+                                        </li>
+                                    @else
+                                        <li class="page-item">
+                                            <a class="page-link" href="{{ $url }}">{{ $page }}</a>
+                                        </li>
+                                    @endif
+                                @endforeach
+
+                                <!-- Next Page Link -->
+                                @if ($communications->hasMorePages())
+                                    <li class="page-item">
+                                        <a class="page-link" href="{{ $communications->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">
+                                            <i class="bi bi-chevron-right"></i>
+                                        </a>
+                                    </li>
+                                @else
+                                    <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
+                                        <span class="page-link" aria-hidden="true"><i class="bi bi-chevron-right"></i></span>
+                                    </li>
+                                @endif
+                            </ul>
+                        </nav>
                     </div>
                 @else
                     <div class="text-center py-5">
