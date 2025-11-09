d1f40059ce0d945dd92684a511687fa101feb821
Shahid Mehmood <smdktk@gmail.com>
2025-11-09 22:46:59 +0500

    Add Utility model import in StudentController for improved functionality

--- app/Http/Controllers/Student/StudentController.php
+++ app/Http/Controllers/Student/StudentController.php
@@ -13,7 +13,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
-
+use App\Models\Utility;
 class StudentController extends Controller
 {
     public function index(){
